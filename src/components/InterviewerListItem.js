bc50a5e16eb12cb14ecc4e8aa96f1612e69f9aea
Stas74 <splotnikov74@gmail.com>
2022-02-23 11:11:18 -0700

    Conditionally add classes and render the interviewer's name

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,17 +4,25 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
   const setInterviewerHandler = () => {
-    props.setInterviewer(props.id)
-  }
+    props.setInterviewer(props.id);
+  };
   return (
-    <li className="interviewers__item" key={props.id} onClick={() => setInterviewerHandler}>
+    <li
+      className={interviewerClass}
+      key={props.id}
+      onClick={() => setInterviewerHandler(props.id)}
+    >
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   );
-};
+}
