5454824f48fd33aafc489e0c2838c40821cd34c1
Stas74 <splotnikov74@gmail.com>
2022-02-23 15:46:21 -0700

    Implemented Interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,28 @@
 import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
 
-import "component/InterviewerList.scss"
+import "./InterviewerList.scss";
 
 export default function InterviewerList(props) {
+  console.log("props", props);
+  const { interviewers, setInterviewer, interviewer } = props;
+
+  const arrInterviewers = interviewers.map((interviewerItem) => {
+    return (
+      <InterviewerListItem
+        key={interviewerItem.id}
+        name={interviewerItem.name}
+        avatar={interviewerItem.avatar}
+        selected={interviewerItem.id === interviewer}
+        setInterviewer={() => setInterviewer(interviewerItem.id)}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{arrInterviewers}</ul>
     </section>
   );
 }
