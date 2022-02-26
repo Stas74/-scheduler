bfef1a1e1cf015f72403311a1472f355bbfbd1cf
Stas74 <splotnikov74@gmail.com>
2022-02-25 20:40:33 -0700

    Helpers folder: selectors - add function getAppointmentsForDay, selectors.test - add tests for this function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,7 @@
+export function getAppointmentsForDay(state, day) {
+  const dayObj = state.days.find((elem) => elem.name === day);
+
+  if (dayObj === undefined) return [];
+
+  return dayObj.appointments.map((app) => state.appointments[app]);
+}
