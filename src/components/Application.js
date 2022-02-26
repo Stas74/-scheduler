6ad9620585edf8c2c4eb174fe493bc62364e7048
Stas74 <splotnikov74@gmail.com>
2022-02-25 18:59:10 -0700

    Add axios in dependencies. Remove hard-coded days data (fetching data through axios from server)

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -64,13 +47,19 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+      console.log("response", response);
+      setDays([...response.data]);
+    });
+  }, []);
 
   const listAppointments = appointments.map((appointment) => (
-    
-    <Appointment key={appointment.id} {...appointment}/>
-   
+    <Appointment key={appointment.id} {...appointment} />
   ));
-  console.log("Spread", {...listAppointments})
+  console.log("Spread", { ...listAppointments });
   return (
     <main className="layout">
       <section className="sidebar">
