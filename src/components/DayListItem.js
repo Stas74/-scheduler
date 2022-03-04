abf870afa49363ad02613bbc61fabe29d776b3dd
Stas74 <splotnikov74@gmail.com>
2022-03-04 15:30:01 -0700

    Add cypress tests.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -21,7 +21,7 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={dayClass} key={props.id} onClick={() => props.setDay(props.name)}>      
+    <li className={dayClass} data-testid="day" key={props.id} onClick={() => props.setDay(props.name)}>      
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
